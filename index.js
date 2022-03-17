f1269465ef2f52717cd00e92daa16c48b270c03c
William Drew <wdrew@governmentcio.com>
2022-03-17 15:27:47 -0400

    Changed port and message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
-const port = 3100;
+const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome!'));
+app.get('/', (req, res) => res.send('Hello from your EC2!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Instance listening on port ${port}...`))
